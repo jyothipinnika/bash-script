783cc6805c5492c61d34451423cc1f441a336f8c
jyothipinnika <60082825+jyothipinnika@users.noreply.github.com>
2020-08-03 14:18:08 -0500

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,9 @@
+conda info
+conda create --name ComputerVision python=2.7 -y
+source activate ComputerVision
+conda install -c menpo opencv -y
+pip install opencv-python
+conda install -c anaconda numpy -y 
+conda install pandas -y 
+conda install -c anaconda sqlalchemy -y
+conda install -c conda- forge matplotlib -y
